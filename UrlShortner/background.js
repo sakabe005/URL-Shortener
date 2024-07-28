8affac560bcbaf12b31675701e75f4fe3f8e9d77
sakabe005 <sakabe1201@gmail.com>
2024-07-28 16:59:52 +0900

    OmniBox関連

--- UrlShortner/background.js
+++ UrlShortner/background.js
@@ -177,3 +177,72 @@ const createSpreadsheet = async (spreadSheetName, defaultSheetName) => {
 
   return spreadsheet.spreadsheetId;
 };
+
+
+/**
+ * OmniBoxに入れられたテキストに合致するものをspreadSheetから探し、見つけてsuggestに表示する機能
+ * @text string OmniBoxに入れられたテキスト
+ * @data string[][] spreadSheetから取得したデータ
+ * @for_suggest string[] suggestに出すためのリスト。短縮URLとoriginal URLを格納
+ */
+chrome.omnibox.onInputChanged.addListener(
+  async function(text, suggest) {
+      const range = `${defaultSheetName}!A:B`;  // データを取得する範囲
+  
+      const data = await getSpreadsheetData(range);
+      if (!data) {
+        console.error('No data found in spreadsheet');
+        return;
+      }
+      let for_suggest = [];
+      for (let row of data) {
+        if (row[1].includes(text)) { 
+          console.log(row[0]);
+          console.log(row[1]);
+          for_suggest.push({content: row[0], description: row[1] + "  |  " + row[0]});  
+        }
+      }
+    
+    suggest(for_suggest);
+ 
+});
+
+/**
+* Ominbox(アドレスバーに入れられた文字列から、オリジナルのURLへ飛ぶ関数)
+* @text {string} Omnibox上のテキストデータ
+*
+*/
+chrome.omnibox.onInputEntered.addListener(
+  async function(text) {
+    console.log(text);
+    chrome.tabs.update({ url: text });
+    
+});
+
+/**
+ * スプレッドシートから、選択範囲のデータを取得する
+ * popup_goですでに使われている関数。こちらでも使用
+ * @param {string} range 取得する範囲
+ * @returns {Promise<string[][]>} 取得したデータ
+ */
+const getSpreadsheetData = async (range) => {
+  const token = await getAuthToken();
+
+  const spreadsheetId = await getSpreadSheetId();
+  const response = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?majorDimension=ROWS`, {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + token,
+      'Content-Type': 'application/json'
+    }
+  });
+
+  if (!response.ok) {
+    const errorDetails = await response.json();
+    console.error('Error fetching spreadsheet data:', errorDetails);
+    return null;
+  }
+
+  const data = await response.json();
+  return data.values;
+};
