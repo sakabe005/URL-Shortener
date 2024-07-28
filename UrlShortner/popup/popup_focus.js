cfdc8bcc77fab90ed962bda5428bd193f95568c8
Hanse Kim <procrustes5@gmail.com>
2024-07-28 12:13:08 +0900

    inputにfocusしているときのキーボード操作

--- UrlShortner/popup/popup_focus.js
+++ UrlShortner/popup/popup_focus.js
@@ -1,4 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+  const input = document.getElementById('shortUrl1');
   // ポップアップが開かれたときに入力欄にフォーカスを当てる
-  document.getElementById('shortUrl1').focus();
+  input.focus();
+  input.addEventListener('keydown', handleKeyDown);
 });
+
+function handleKeyDown(event) {
+  if (event.isComposing) return;
+
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    if (event.ctrlKey || event.metaKey) {
+      document.getElementById('make').click();
+    } else {
+      document.getElementById('go').click();
+    }
+  }
+};
