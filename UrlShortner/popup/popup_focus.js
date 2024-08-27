c8f5069101a64affc9080f4fdc0e4fa9853c320e
lunaInsidious <itsukitakatsuno@gmail.com>
2024-08-27 20:32:27 +0900

    設定画面追加

--- UrlShortner/popup/popup_focus.js
+++ UrlShortner/popup/popup_focus.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
   document.addEventListener('keydown', handleUrlSelectionKeyDown);
 });
 
-export function handleKeyDown(event) {
+function handleKeyDown(event) {
   if (event.isComposing) return;
 
   if (event.key === 'Enter') {
